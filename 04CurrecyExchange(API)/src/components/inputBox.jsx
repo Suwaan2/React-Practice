bb2385835d4850b129f01b04403b0e1072ec03c0
Suan KC <kcsuan424@gmail.com>
2024-12-18 17:02:13 +0545

    input

---
+++ 04CurrecyExchange(API)/src/components/inputBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function inputBox({
+    //Pre Defined Properties of the Components pre-default Props:
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = 'usd',
+    currencyDisable = false,
+    amountDiable = false,
+    className = "",
+}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default inputBox
