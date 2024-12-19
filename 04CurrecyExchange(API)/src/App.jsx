a41102bc3581b02806ca0d9de2ccc1b4a3bf7316
Suan KC <kcsuan424@gmail.com>
2024-12-19 19:47:39 +0545

    inputBox Component added(competed)

--- 04CurrecyExchange(API)/src/App.jsx
+++ 04CurrecyExchange(API)/src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import InputBox from './components/InputBox.jsx'
+import { use } from 'react'
+import useCurrency from './Hooks/useCurrency.js'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmt , setConvertedAmt] = useState(0)
+
+  const currencyInfo = useCurrency(from)
 
   return (
+    <>
   <h1 className=' text-4xl'>Currency Converter</h1>
+   
+    </>
+    
   )
 }
 
