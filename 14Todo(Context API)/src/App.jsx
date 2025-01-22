240e7e7590d68715ed00e970f3cd33dc3fb2db5a
Suan KC <kcsuan424@gmail.com>
2025-01-22 18:51:39 +0545

    starting react second part

---
+++ 14Todo(Context API)/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+  <>
+  <h1 className='bg-black text-4xl text-white'>TODO LIST with Context API</h1>
+  </>
+  )
+}
+
+export default App
